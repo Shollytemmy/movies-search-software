98795c435f9c69f73087ed966f5429d2aeb783a5
Bunyameen <bunyameen@gmail.com>
2022-07-28 21:00:04 +0100

    work in progress

---
+++ src/context/shows/ShowsState.js
@@ -0,0 +1,22 @@
+import {useReducer} from 'react'
+import showsContext from './showsContext'
+import ShowsReducer from './showsReducer'
+import {
+    SEARCH_SHOWS,
+    SET_LOADING,
+    SET_SHOW_DETAIL,
+    CLEAR_SHOW_DETAIL,
+    SET_ALERT,
+    REMOVE_ALERT
+    
+
+} from '../types'
+
+
+import React from 'react'
+
+export const ShowsState = () => {
+  return (
+    <div>ShowsState</div>
+  )
+}
---
+++ src/context/types.js
@@ -0,0 +1,9 @@
+export const SEARCH_SHOWS = 'SEARCH_SHOWS'
+export const SET_LOADING = 'SET_LOADING'
+export const SET_SHOW_DETAIL = 'SET_SHOW_DETAIL'
+export const CLEAR_SHOW_DETAIL = 'CLEAR_SHOW_DETAIL'
+
+// Alerts
+
+export const SET_ALERT = 'SET_ALERT'
+export const REMOVE_ALERT = 'REMOVE_ALERT'
