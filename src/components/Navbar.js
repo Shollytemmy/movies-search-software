02f298f0d866c3841b36254d6a6a453fabe2c245
Bunyameen <bunyameen@gmail.com>
2022-07-28 13:00:21 +0100

    Added rect-icns library and navigation links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 // import { IconName } from "react-icons/fa"; SiThemoviedatabase
+import {SiThemoviedatabase} from 'react-icons/si'
 
 export const Navbar = () => {
   return (
@@ -8,6 +9,18 @@ export const Navbar = () => {
         <div className="container">
             <nav>
                 <Link to='/' >
+                    <h3>
+                        <SiThemoviedatabase /> Movies Search App
+                    </h3>
+                    <ul>
+                        <li>
+                            <Link to="/" >Home</Link>
+                        </li>
+                        
+                        <li>
+                            <Link to='/about'>About</Link>
+                        </li>
+                    </ul>
 
                 </Link>
             </nav>
